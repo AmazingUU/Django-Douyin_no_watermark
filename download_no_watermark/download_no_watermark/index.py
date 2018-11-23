583d8b7af2e5485a5127875db78c90c73f9d8abb
AmazingUU <AmazingUU@163.com>
2018-11-23 21:26:56 +0800

    2018/11/23 21:26

--- download_no_watermark/download_no_watermark/index.py
+++ download_no_watermark/download_no_watermark/index.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+
+from download_no_watermark.download_no_watermark import Downloader
+
 
 def index(request):
-    return render(request, 'index.html')
+    context = {}
+    if request.POST:
+        share_url = request.POST['search']
+        downloader = Downloader()
+        download_url = downloader.run(share_url)
+        if download_url:
+            return HttpResponseRedirect(download_url)
+            # context['rlt'] = '下载成功'
+        else:
+            context['rlt'] = '下载失败'
+    return render(request, 'index.html',context)
