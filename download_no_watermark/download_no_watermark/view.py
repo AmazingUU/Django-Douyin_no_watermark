0492869f3df77106d1cd6c825919accbb179bcec
AmazingUU <AmazingUU@163.com>
2018-11-25 20:59:12 +0800

    2018/11/25 20:59

--- download_no_watermark/download_no_watermark/view.py
+++ download_no_watermark/download_no_watermark/view.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from download_no_watermark.download_no_watermark import Downloader
 
 def hello(request):
     context = {}
-    context['hello'] = 'Hello World!'
-    return render(request, 'hello.html', context)
+    if request.POST:
+        share_url = request.POST['search']
+        downloader = Downloader()
+        download_url,desc = downloader.run(share_url)
+        if download_url:
+            context['download_url'] = download_url
+            context['desc'] = desc
+            # return HttpResponseRedirect(download_url)
+        else:
+            context['download_url'] = '下载失败'
+    return render(request, 'hello.html',context)
